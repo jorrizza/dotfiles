f700c22f73433b22b099a0a71ae05a794ff4cc01
Joris van Rooij <jorrizza@jrrzz.net>
2011-07-12 00:17:44 +0200

    Send all google searches to duckduckgo.com

--- .js/google.com.js
+++ .js/google.com.js
@@ -1,1 +1,10 @@
+//Google Troll Face
 $('#lga > img:first').attr('src', 'http://www.trollface.net/trollface.png').attr('height', '').css('margin-top', '-120px');
+
+//Secretly send search query to duckduckgo.com
+$('input[type=text][name=q]').closest('form').attr('action', 'https://duckduckgo.com/');
+
+//Make sure autocomplete is turned off, otherwise our trick won't work
+if (!window.location.search.match('complete=0')) {
+  window.location.replace('http://www.google.com/webhp?complete=0');
+}

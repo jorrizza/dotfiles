7051909047a73752d62feb4867d33d878ab681f5
Joris van Rooij <jorrizza@jrrzz.net>
2011-12-11 00:57:10 +0100

    Because people kept asking.

--- .js/rijksoverheid.nl.js
+++ .js/rijksoverheid.nl.js
@@ -1,1 +1,2 @@
+// http://www.rijksoverheid.nl/ministeries/venj/nieuws/2011/06/29/amerikaanse-steun-voor-auteursrechtplannen-teeven.html
 $('.content-image-left > img:first').attr('src', 'http://i.imgur.com/kQZtY.png');

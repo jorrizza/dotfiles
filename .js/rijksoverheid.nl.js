79df77da2aced373402465ad85cb22d81381c6e9
Joris van Rooij <jorrizza@jrrzz.net>
2015-07-21 11:47:08 +0200

    Dotjs is no longer used.

--- .js/rijksoverheid.nl.js
+++
@@ -1,2 +0,0 @@
-// http://www.rijksoverheid.nl/ministeries/venj/nieuws/2011/06/29/amerikaanse-steun-voor-auteursrechtplannen-teeven.html
-$('.content-image-left > img:first').attr('src', 'http://i.imgur.com/kQZtY.png');

1846f76234d96d544f606f2d0651bb7f4de75372
Joris van Rooij <jorrizza@jrrzz.net>
2021-03-15 14:35:11 +0100

    code-server startup and i3bar updates

--- src/i3bar/main.go
+++ src/i3bar/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"os/user"
 	"path/filepath"
+	"runtime"
 	"time"
 
 	"barista.run"
 	"barista.run/bar"
 	"barista.run/base/click"
 	"barista.run/base/watchers/netlink"
-	"barista.run/colors"
 	"barista.run/format"
 	"barista.run/modules/battery"
 	"barista.run/modules/clock"
@@ -20,8 +20,6 @@ import (
 	"barista.run/outputs"
 	"barista.run/pango"
 	"barista.run/pango/icons/fontawesome"
-
-	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
 var spacer = pango.Text(" ").XXSmall()
@@ -51,21 +49,6 @@ func home(path string) string {
 func main() {
 	fontawesome.Load(home("src/Font-Awesome"))
 
-	colors.LoadBarConfig()
-	bg := colors.Scheme("background")
-	fg := colors.Scheme("statusline")
-	if fg != nil && bg != nil {
-		iconColor := fg.Colorful().BlendHcl(bg.Colorful(), 0.5).Clamped()
-		colors.Set("dim-icon", iconColor)
-		_, _, v := fg.Colorful().Hsv()
-		if v < 0.3 {
-			v = 0.3
-		}
-		colors.Set("bad", colorful.Hcl(40, 1.0, v).Clamped())
-		colors.Set("degraded", colorful.Hcl(90, 1.0, v).Clamped())
-		colors.Set("good", colorful.Hcl(120, 1.0, v).Clamped())
-	}
-
 	localtime := clock.Local().
 		Output(time.Second, func(now time.Time) bar.Output {
 			return outputs.Pango(
@@ -92,13 +75,8 @@ func main() {
 			iconName += "-empty"
 		}
 		out := outputs.Pango(pango.Icon("fa-"+iconName), disp)
-		switch {
-		case percentage <= 5:
+		if percentage <= 5 {
 			out.Urgent(true)
-		case percentage <= 15:
-			out.Color(colors.Scheme("bad"))
-		case percentage <= 25:
-			out.Color(colors.Scheme("degraded"))
 		}
 		return out
 	}
@@ -129,19 +107,11 @@ func main() {
 
 	loadAvg := sysinfo.New().Output(func(s sysinfo.Info) bar.Output {
 		out := outputs.Pango(pango.Icon("fa-microchip"), spacer, pango.Textf("%0.2f %0.2f", s.Loads[0], s.Loads[2]))
-		// Load averages are unusually high for a few minutes after boot.
-		if s.Uptime < 10*time.Minute {
-			// so don't add colours until 10 minutes after system start.
-			return out
-		}
-		switch {
-		case s.Loads[0] > 128, s.Loads[2] > 64:
+
+		if s.Loads[0] > float64(runtime.NumCPU()) {
 			out.Urgent(true)
-		case s.Loads[0] > 64, s.Loads[2] > 32:
-			out.Color(colors.Scheme("bad"))
-		case s.Loads[0] > 32, s.Loads[2] > 16:
-			out.Color(colors.Scheme("degraded"))
 		}
+
 		return out
 	})
 
@@ -151,12 +121,6 @@ func main() {
 		switch {
 		case freeGigs < 0.5:
 			out.Urgent(true)
-		case freeGigs < 1:
-			out.Color(colors.Scheme("bad"))
-		case freeGigs < 2:
-			out.Color(colors.Scheme("degraded"))
-		case freeGigs > 12:
-			out.Color(colors.Scheme("good"))
 		}
 		return out
 	})
@@ -180,17 +144,21 @@ func main() {
 				return nil
 			}
 
-			out := outputs.Pango(
-				pango.Icon("fa-wifi"),
-				spacer,
-				pango.Textf("%s (%0.2f GHz)", i.SSID, i.Frequency.Gigahertz()),
-			)
+			var out *bar.Segment
 
-			switch {
-			case i.Connected():
-				out.Color(colors.Scheme("good"))
-			case i.Connecting():
-				out.Color(colors.Scheme("degraded"))
+			if i.State == netlink.Up {
+				out = outputs.Pango(
+					pango.Icon("fa-wifi"),
+					spacer,
+					pango.Textf("%s (%0.2f GHz)", i.SSID, i.Frequency.Gigahertz()),
+				)
+			} else {
+				out = outputs.Pango(
+					pango.Icon("fa-wifi"),
+					spacer,
+					pango.Text("down"),
+				)
+				out.Urgent(true)
 			}
 
 			return out

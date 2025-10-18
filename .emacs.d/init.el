0a708f4be26adca12bb977aeb17eff8c36fd560a
Joris van Rooij <jorrizza@jrrzz.net>
2025-10-18 19:41:50 +0200

    rewrote emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,43 +1,65 @@
+;;
 ;; Package management
+;;
+
+
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")))
 (package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
-(defvar my-packages '(smooth-scrolling yaml-mode
-		      graphviz-dot-mode tramp markdown-mode
-		      dockerfile-mode ag toml-mode company
-		      project xref eldoc go-mode zig-mode
-		      solarized-theme diff-hl web-mode)
-  "Nice packages I depend upon.")
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
 
-;; We don't need these
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;; Unique double filenames
-(require 'uniquify)
+;; Language modes
+(add-to-list 'package-selected-packages 'dockerfile-mode)
+(add-to-list 'package-selected-packages 'markdown-mode)
+(add-to-list 'package-selected-packages 'toml-mode)
+(add-to-list 'package-selected-packages 'yaml-mode)
+
+;; Auto-complete
+(add-to-list 'package-selected-packages 'company)
+
+;; Inline docs
+(add-to-list 'package-selected-packages 'eldoc)
+
+;; Pretty things
+(add-to-list 'package-selected-packages 'ido)
+(add-to-list 'package-selected-packages 'smooth-scrolling)
+(add-to-list 'package-selected-packages 'solarized-theme)
+
+(package-install-selected-packages)
+
+
+;;
+;; Look & Feel
+;;
+
 
 ;; Set windows title
 (setq frame-title-format '("" "%b - Emacs " emacs-version))
 
-;; Default mode
-(setq initial-major-mode 'fundamental-mode)
+;; Line numbering
+(global-display-line-numbers-mode)
+(setq-default display-line-numbers-width-start t)
 
-;; Fix archaic defaults
-(setq sentence-end-double-space nil)
+;; Show column number
+(column-number-mode 1)
 
-;; Make right-click do something sensible
-(when (display-graphic-p)
-  (context-menu-mode))
+;; Highlight matching bracket
+(show-paren-mode 1)
 
-;; Save history of minibuffer
-(savehist-mode)
+;; Line wrapping
+(set-default 'truncate-lines t)
+(global-visual-line-mode +1)
+
+;; We don't need these
+(setq inhibit-startup-message t)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
 
 ;; Nice colors
 (load-theme 'solarized-dark t)
@@ -46,13 +68,85 @@
 (add-to-list 'default-frame-alist '(font . "Terminus-8"))
 (set-face-attribute 'default t :font "Terminus-8")
 
-;; We use versioning. Backup files are not needed.
-(setq make-backup-files nil)
+;; Smooth scrolling
+(smooth-scrolling-mode)
 
-;; We also never share files on the same machine
-(setq create-lockfiles nil)
+;; Version control sidebar highlight
+(global-diff-hl-mode)
 
-;; Fill mode
+;; Show stray whitespace
+(setq-default show-trailing-whitespace t)
+(setq-default indicate-empty-lines t)
+
+;; Highlight current line
+(global-hl-line-mode +1)
+
+;; Allow ElDoc to use multiple lines in the echo area
+(setq-default eldoc-echo-area-use-multiline-p t)
+
+
+;;
+;; Editor Behavior
+;;
+
+
+;; Make M-arrow work for window switching
+(windmove-default-keybindings 'meta)
+
+;; Automatically insert matching bracket or quote
+(electric-pair-mode +1)
+
+;; Automatically indent every new line
+(electric-indent-mode +1)
+
+;; For (stupid) indent based languages, disable electric indent
+(defun ignore-electric-indent (char)
+  "Ignore electric indent for some modes"
+  (if (member major-mode '(python-mode yaml-mode))
+      `no-indent'
+    nil))
+(add-hook 'electric-indent-functions 'ignore-electric-indent)
+
+;; Use UTF-8 already
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
+(setq-default default-buffer-file-coding-system 'utf-8-unix)
+(setq-default default-keyboard-coding-system 'utf-8-unix)
+(setq-default default-terminal-coding-system 'utf-8-unix)
+
+;; Every file should end with a newline
+(setq-default require-final-newline t)
+
+;; Unique double filenames as buffers
+(require 'uniquify)
+
+;; Default mode
+(setq initial-major-mode 'fundamental-mode)
+
+;; Fix archaic defaults
+(setq-default sentence-end-double-space nil)
+(setq-default make-backup-files nil)
+(setq-default create-lockfiles nil)
+
+;; Make right-click do something sensible
+(when (display-graphic-p)
+  (context-menu-mode))
+
+;; Save history of minibuffer
+(savehist-mode)
+
+;; Keep buffers in sync with filesystem
+(setq-default auto-revert-interval 1)
+(setq-default auto-revert-check-vc-info t)
+(global-auto-revert-mode t)
+
+;; Overwrite selected text
+(delete-selection-mode +1)
+
+;; Remember where we were in files
+(save-place-mode +1)
+
+;; Nicely fill out chunks of text up to the fill-column with M-q
 (setq-default fill-column 79)
 (defun unfill-paragraph (&optional region)
   "Takes a multi-line paragraph and makes it into a single line of text."
@@ -63,164 +157,114 @@
     (fill-paragraph nil region)))
 (define-key global-map "\M-Q" 'unfill-paragraph)
 
-;; Damn you tabs!
+;; Auto completion within buffers
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Auto completion within the minibuffer
+(setq-default ido-enable-flex-matching t)
+(setq-default ido-everywhere t)
+(ido-mode t)
+
+;; Turn tabs into four spaces (go will have its own config later on)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 
-;; Line numbering
-(global-display-line-numbers-mode)
+;; Use treesitter by default
+(setq major-mode-remap-alist
+      '((go-mode . go-ts-mode)
+        (python-mode . python-ts-mode)
+        (yaml-mode . yaml-ts-mode)
+        (js-mode . js-ts-mode)))
 
-;; Show column number
-(column-number-mode 1)
-
-;; Highlight matching bracket
-(show-paren-mode 1)
-
-;; Make M-arrow work for window switching
-(windmove-default-keybindings 'meta)
-
-;; Electric features that rock
-(electric-pair-mode +1)
-(electric-indent-mode +1)
-
-;; For indent based languages, disable electric indent
-(defun ignore-electric-indent (char)
-  "Ignore electric indent for some modes"
-  (if (member major-mode '(python-mode yaml-mode))
-      `no-indent'
-    nil))
-(add-hook 'electric-indent-functions 'ignore-electric-indent)
-
-;; Function to replace electric indent for indent based languages
-(defun set-newline-and-indent ()
-  "Map the return key with `newline-and-indent'"
-  (local-set-key (kbd "RET") 'newline-and-indent))
-
-;; Keep buffers in sync with filesystem
-(setq auto-revert-interval 1)
-(setq auto-revert-check-vc-info t)
-(global-auto-revert-mode t)
-
-;; Auto completion
-(add-hook 'after-init-hook 'global-company-mode)
-
-;; Auto completion for minibuf
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode t)
-
-;; Settings for ag
-(setq ag-reuse-window 't)
-(setq ag-reuse-buffers 't)
-
-;; VC sidebar highlight
-(global-diff-hl-mode)
-
-;; Tramp
-(setq tramp-default-method "ssh")
-
-;; Auto-indent yanked code
-(dolist (command '(yank yank-pop))
-   (eval `(defadvice ,command (after indent-region activate)
-            (and (not current-prefix-arg)
-                 (member major-mode '(emacs-lisp-mode lisp-mode
-                                                      python-mode
-                                                      c-mode
-                                                      go-mode))
-                 (let ((mark-even-if-inactive transient-mark-mode))
-                   (indent-region (region-beginning) (region-end) nil))))))
+;; Stolen list of working treesitter grammars from combobulate
+;; https://github.com/mickeynp/combobulate/blob/master/README.rst
+(dolist (grammar
+         ;; Note the version numbers. These are the versions that
+         ;; are known to work with Combobulate *and* Emacs.
+         '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+           (go . ("https://github.com/tree-sitter/tree-sitter-go" "v0.20.0"))
+           (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+           (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
+           (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+           (markdown . ("https://github.com/ikatyang/tree-sitter-markdown" "v0.7.1"))
+           (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+           (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
+           (toml . ("https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1"))
+           (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+           (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+           (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
+  ;; (add-to-list 'treesit-language-source-alist grammar)
+  ;; Only install `grammar' if we don't already have it
+  ;; installed. However, if you want to *update* a grammar then
+  ;; this obviously prevents that from happening.
+  (unless (treesit-language-available-p (car grammar))
+    (treesit-install-language-grammar (car grammar))))
 
 
-;; Remove trailing white space
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;;
+;; Package-specific Config
+;;
 
-;; My spelling is sub-par
-(add-hook 'text-mode-hook 'flyspell-mode)
-(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+;; LSP client
+;; We want to both organize imports and format code before saving files
+(defun eglot-format-and-imports-on-save ()
+  "Run both formatting and import organization before saving."
+  (when eglot--managed-mode
+    (when (eglot--server-capable :documentFormattingProvider)
+      (eglot-format-buffer))
+    (when (eglot--server-capable :codeActionProvider)
+      (eglot-code-action-organize-imports))))
 
-;; Tree sitter stuff (not enabled in Debian yet)
-;; (setq major-mode-remap-alist
-;;       '((yaml-mode . yaml-ts-mode)
-;;         (bash-mode . bash-ts-mode)
-;;         (js2-mode . js-ts-mode)
-;;         (typescript-mode . typescript-ts-mode)
-;;         (json-mode . json-ts-mode)
-;;         (css-mode . css-ts-mode)
-;;         (python-mode . python-ts-mode)
-;;         (c-mode . c-ts-mode)
-;;         (go-mode . go-ts-mode)))
-
-;; Eglot fixes
 (use-package eglot
   :custom
   (eglot-send-changes-idle-time 0.1)
 
   :config
   (fset #'jsonrpc--log-event #'ignore)
+
+  :hook
+  (eglot-managed-mode . (lambda ()
+                          (add-hook 'before-save-hook #'eglot-format-and-imports-on-save nil 'local)))
   )
 
-;; Python
-(add-hook 'python-mode-hook
-          'eglot-ensure)
-(add-hook 'python-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook 'eglot-format nil t)
-            (set-newline-and-indent)))
-
 ;; Go
-(add-hook 'go-mode-hook
-          'eglot-ensure)
-(add-hook 'go-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook 'eglot-format nil t)
-            (add-hook 'before-save-hook (lambda ()
-                                          (call-interactively 'eglot-code-action-organize-imports))
-                      nil t)))
+(use-package go-ts-mode
+  :ensure t
 
-;; Zig
-(add-hook 'zig-mode-hook
-          'eglot-ensure)
-(add-hook 'zig-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook 'eglot-format nil t)))
+  :mode
+  "\\.go\\'"
+
+  :init
+  (setq go-ts-mode-indent-offset 4) ;; Because our tab-width is 4
+
+  :hook
+  (go-ts-mode . (lambda ()
+                  (eglot-ensure))))
+
+;; Python
+(use-package python-ts-mode
+  ;; :ensure t
+
+  :mode
+  "\\.py\\'"
+
+  ;; :init
+  ;; (setq something)
+
+  :hook
+  (python-ts-mode . (lambda ()
+                      (eglot-ensure))))
 
 ;; Web
-(add-to-list 'auto-mode-alist '("\\.gohtml\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (electric-pair-mode -1)
-            (setq web-mode-markup-indent-offset 2)
-            (setq web-mode-css-indent-offset 2)
-            (setq web-mode-code-indent-offset 2)))
+(use-package web-mode
+  :ensure t
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(js-indent-level 2)
- '(markdown-code-lang-modes
-   '(("ocaml" . tuareg-mode) ("elisp" . emacs-lisp-mode) ("ditaa" . artist-mode)
-     ("asymptote" . asy-mode) ("dot" . fundamental-mode) ("sqlite" . sql-mode)
-     ("calc" . fundamental-mode) ("C" . c-mode) ("cpp" . c++-mode)
-     ("C++" . c++-mode) ("screen" . shell-script-mode) ("shell" . sh-mode)
-     ("bash" . sh-mode) ("yaml" . yaml-mode)))
- '(markdown-enable-highlighting-syntax t)
- '(markdown-fontify-code-blocks-natively t)
- '(package-selected-packages
-   '(ag company diff-hl dockerfile-mode go-mode graphviz-dot-mode just-mode
-        markdown-mode smooth-scrolling solarized-theme toml-mode web-mode
-        yaml-mode zig-mode))
- '(safe-local-variable-values
-   '((format-all-formatters ("Python" black)) (python-sort-imports-on-save t)))
- '(warning-suppress-log-types '((comp)))
- '(warning-suppress-types '((comp))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "PfEd" :family "Terminus"))))
- '(fixed-pitch ((t nil)))
- '(fixed-pitch-serif ((t nil))))
+  :mode
+  (("\\.html\\'" . web-mode)
+   ("\\.gohtml\\'" . web-mode)
+   ("\\.tpl\\'" . web-mode)
+   ("\\.erb\\'" . web-mode)
+   ("\\.djhtml\\'" . web-mode))
+
+  :init
+  (setq web-mode-enable-current-column-highlight t))

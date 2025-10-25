983e8f4710fe71166a72010328669e3bc642e8d8
Joris van Rooij <jorrizza@jrrzz.net>
2025-10-25 13:42:58 +0200

    cleaned up the tree-sitter situation with tree-sitter-langs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,14 +4,17 @@
 
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")))
+			             ("melpa" . "https://melpa.org/packages/")
+			             ("org" . "https://orgmode.org/elpa/")))
 (package-initialize)
 
 ;; Bootstrap `use-package`
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+;; Tree-sitter language pack
+(add-to-list 'package-selected-packages 'tree-sitter-langs)
 
 ;; Language modes
 (add-to-list 'package-selected-packages 'dockerfile-mode)
@@ -76,7 +79,6 @@
 
 ;; Show stray whitespace
 (setq-default show-trailing-whitespace t)
-(setq-default indicate-empty-lines t)
 
 ;; Highlight current line
 (global-hl-line-mode +1)
@@ -169,50 +171,13 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 
-;; Use treesitter by default
-(setq major-mode-remap-alist
-      '((go-mode . go-ts-mode)
-        (python-mode . python-ts-mode)
-        (yaml-mode . yaml-ts-mode)
-        (js-mode . js-ts-mode)))
+;; Use tree-sitter by default
+(global-tree-sitter-mode)
 
 
 ;;
 ;; Package-specific Config
 ;;
-
-
-;; Stolen list of working treesitter grammars from combobulate
-;; https://github.com/mickeynp/combobulate/blob/master/README.rst
-(use-package treesit
-  :preface
-  (defun mp-setup-install-grammars ()
-    "Install Tree-sitter grammars if they are absent."
-    (interactive)
-    (dolist (grammar
-             ;; Note the version numbers. These are the versions that
-             ;; are known to work with Combobulate *and* Emacs.
-             '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
-               (go . ("https://github.com/tree-sitter/tree-sitter-go" "v0.20.0"))
-               (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
-               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
-               (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
-               (markdown . ("https://github.com/ikatyang/tree-sitter-markdown" "v0.7.1"))
-               (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
-               (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
-               (toml . ("https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1"))
-               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
-               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
-               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
-      (add-to-list 'treesit-language-source-alist grammar)
-      ;; Only install `grammar' if we don't already have it
-      ;; installed. However, if you want to *update* a grammar then
-      ;; this obviously prevents that from happening.
-      (unless (treesit-language-available-p (car grammar))
-        (treesit-install-language-grammar (car grammar)))))
-
-  :config
-  (mp-setup-install-grammars))
 
 
 ;; LSP client
